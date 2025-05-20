db298f75f64d3be43fd627dc25831945e47c989e
Fenosoa Randrianjatovo <98966847+FenosoaRandrianjatovo@users.noreply.github.com>
2025-05-20 09:58:42 -0400

    nwe feature

---
+++ run_xenium.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --job-name=run_pipeline_TMA
+#SBATCH --account=def-amadou
+#SBATCH --mem=500G
+#SBATCH --time=0-20:00:00
+#SBATCH --cpus-per-task=30
+#SBATCH --mail-user=fenosoaammi@gmail.com
+#SBATCH --mail-type=END,FAIL
+#SBATCH --output=/home/fenosoa/projects/def-salehlab-ab/fenosoa/code_source_for_Pipeline/Pipeline_For_TMA_Xenium/logs/run_pipeline_TMA_%j.out
+#SBATCH --error=/home/fenosoa/projects/def-salehlab-ab/fenosoa/code_source_for_Pipeline/Pipeline_For_TMA_Xenium/logs/run_pipeline_TMA_%j.err
+
+
+# Output message
+echo "Job is starting..."
+
+# Load the necessary environment and Python module
+
+module load StdEnv/2023
+module load  gcc/12.3  r/4.4.0
+
+echo "Rscript  is starting..."
+
+Rscript pipeline_first_draft.R
+
+echo "Job finished."
