e0c8caf4164ca4cf329904fb59baca4aec4c755c
Fenosoa Randrianjatovo <fenosoa@beluga2.int.ets1.calculquebec.ca>
2025-05-23 08:35:07 -0400

    reun_xe

--- run_xenium.sh
+++ run_xenium.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=run_pipeline_TMA
+#SBATCH --job-name=run_TMA
 #SBATCH --account=def-amadou
-#SBATCH --mem=500G
+#SBATCH --mem=64G
 #SBATCH --time=0-20:00:00
 #SBATCH --cpus-per-task=30
 #SBATCH --mail-user=fenosoaammi@gmail.com
