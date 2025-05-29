a5507bd0bf82ca93bbba8691f0659874f98f8a53
Fenosoa Randrianjatovo <98966847+FenosoaRandrianjatovo@users.noreply.github.com>
2025-05-29 11:19:06 -0400

    750Gb

--- run_xenium.sh
+++ run_xenium.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=run_TMA
 #SBATCH --account=def-amadou
-#SBATCH --mem=528G
+#SBATCH --mem=750G
 #SBATCH --time=0-20:00:00
-#SBATCH --cpus-per-task=30
+#SBATCH --cpus-per-task=40
 #SBATCH --mail-user=fenosoaammi@gmail.com
 #SBATCH --mail-type=END,FAIL
 #SBATCH --output=/home/fenosoa/projects/def-salehlab-ab/fenosoa/code_source_for_Pipeline/Pipeline_For_TMA_Xenium/logs/run_pipeline_TMA_%j.out
