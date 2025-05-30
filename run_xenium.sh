3eaa0b1ebf425406af056d5bff3e8a778fb5d919
Fenosoa Randrianjatovo <98966847+FenosoaRandrianjatovo@users.noreply.github.com>
2025-05-29 20:37:51 -0400

    750

--- run_xenium.sh
+++ run_xenium.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=run_TMA
 #SBATCH --account=def-amadou
-#SBATCH --mem=800G
+#SBATCH --mem=750G
 #SBATCH --time=0-20:00:00
 #SBATCH --cpus-per-task=40
 #SBATCH --mail-user=fenosoaammi@gmail.com
