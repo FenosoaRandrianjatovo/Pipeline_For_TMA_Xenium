52c5e4ab4c9ca624ed97538cfb0cf84fb1a50fe8
Fenosoa Randrianjatovo <98966847+FenosoaRandrianjatovo@users.noreply.github.com>
2025-05-29 20:33:46 -0400

    dd

--- pipeline_first_draft.R
+++ pipeline_first_draft.R
@@ -7,7 +7,7 @@ library(patchwork)
 library(RColorBrewer)
 library(patchwork)
 
-plan("multisession", workers = 30)
+plan("multisession", workers = 25)
 options(future.globals.maxSize = 700 * 1024^3) 
 
 # remotes::install_version(package = 'Seurat', version = package_version('5.2.0'))
@@ -35,13 +35,21 @@ print("Save Genes as a text file")
 # Save as a text file
 write.table(genes, file = "genes_TMA.txt", row.names = FALSE, col.names = FALSE, quote = FALSE)
 
+
 print("==================================================================================")
 plot0 <- ImageDimPlot(xenium.obj, fov = "fov", molecules = c("RETREG1",   "RETREG3", "PIMREG", "FOXRED1"), nmols = 20000)
 ggsave("/home/fenosoa/projects/def-salehlab-ab/fenosoa/code_source_for_Pipeline/ImageDimPlot_TMA_RETREG1_FOXRED1_v1.png", plot = plot0, width = 20, height = 15, dpi = 300)
 print("==================================================================================")
 
 print("SCTransform is running")
-xenium.obj <- SCTransform(xenium.obj, assay = "Xenium")
+# xenium.obj <- SCTransform(xenium.obj, assay = "Xenium")
+
+xenium.obj <- SCTransform(
+  xenium.obj,
+  assay = "Xenium",
+  layer = "counts"      # use `layer` instead of deprecated `slot`
+)
+
 #> dim(xenium.obj@assays$SCT@counts)
 #[1]   248 36553
 
