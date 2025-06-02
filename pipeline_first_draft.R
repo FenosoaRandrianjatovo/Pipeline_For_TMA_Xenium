e9e3789bdc287bb4bde9d112837979a8d6fccebd
Fenosoa Randrianjatovo <98966847+FenosoaRandrianjatovo@users.noreply.github.com>
2025-06-02 10:33:59 -0400

    coun

--- pipeline_first_draft.R
+++ pipeline_first_draft.R
@@ -7,7 +7,6 @@ library(patchwork)
 library(RColorBrewer)
 library(patchwork)
 
-# plan("multisession", workers = 25)
 
 cl <- parallelly::makeClusterPSOCK(25, outfile="")
 plan(cluster, workers = cl)
@@ -48,14 +47,14 @@ xenium.obj <- temp_env$xenium.obj
 
 print("==================================================================================")
 
-# genes <- rownames(xenium.obj)
+genes <- rownames(xenium.obj)
 
-# print("Save Genes as a text file")
-# # Save as a text file
-# write.table(genes, file = "genes_TMA.txt", row.names = FALSE, col.names = FALSE, quote = FALSE)
+print("Save Genes as a text file")
+# Save as a text file
+write.table(genes, file = "genes_TMA.txt", row.names = FALSE, col.names = FALSE, quote = FALSE)
 
 
-# print("==================================================================================")
+print("==================================================================================")
 # plot0 <- ImageDimPlot(xenium.obj, fov = "fov", molecules = c("RETREG1",   "RETREG3", "PIMREG", "FOXRED1"), nmols = 20000)
 # ggsave("/home/fenosoa/projects/def-salehlab-ab/fenosoa/code_source_for_Pipeline/ImageDimPlot_TMA_RETREG1_FOXRED1_v1.png", plot = plot0, width = 20, height = 15, dpi = 300)
 print("==================================================================================")
