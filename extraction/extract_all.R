d440eec7fc20705e5036b4e80e0f63aff15ff6e0
Fenosoa Randrianjatovo <98966847+FenosoaRandrianjatovo@users.noreply.github.com>
2025-12-19 14:43:46 -0500

    Fix print statements for clarity in extract_all.R

--- extraction/extract_all.R
+++ extraction/extract_all.R
@@ -25,22 +25,22 @@ print(length(groups))
 n <- length(groups)
 
 
-print("# 1. Retrieve the SCT data matrix")
+print("#Retrieve the SCT data matrix")
 counts_mat <- GetAssayData(xenium.obj, assay = "SCT", slot = "data")
 
-print("# 2. Select all cell barcodes (no downsampling)")
+print("#  Select all cell barcodes (no downsampling)")
 selected_cells <- colnames(counts_mat)
 
-print("# 3. Subset the matrix to all cells (genes stay the same)")
+print("# Subset the matrix to all cells (genes stay the same)")
 counts_sub <- counts_mat[, selected_cells]
 
-print("# 4. Transpose & coerce to a dense data.frame")
+print("# Transpose & coerce to a dense data.frame")
 counts_df <- as.data.frame(t(as.matrix(counts_sub)))
 
-print("# 5. Add the corresponding cluster labels for every cell")
+print("# Add the corresponding cluster labels for every cell")
 counts_df$cluster <- xenium.obj@meta.data[selected_cells, "seurat_clusters"]
 
-print("# 6. Write out to CSV file")
+print("# Write out to CSV file")
 write.csv(
   counts_df,
   file = "TMA_counts_with_clusters_SCT_data_AllCells.csv",
