22ac9ecb9ea48beb8647ad4b6b80e7a10c462846
Fenosoa Randrianjatovo <98966847+FenosoaRandrianjatovo@users.noreply.github.com>
2025-12-19 14:54:21 -0500

    Reduce memory allocation from 250G to 150G

--- extraction/run_extra.sh
+++ extraction/run_extra.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=run_extrac
 #SBATCH --account=def-amadou
-#SBATCH --mem=250G
+#SBATCH --mem=150G
 #SBATCH --time=0-10:00:00
 #SBATCH --cpus-per-task=10
-#SBATCH --mail-user=fenosoaammi@gmail.com
 #SBATCH --mail-type=ALL
 #SBATCH --output=/home/fenosoa/projects/def-salehlab-ab/fenosoa/code_source_for_Pipeline/Pipeline_For_TMA_Xenium/logs/run_pipeline_TMA_%j.out
 #SBATCH --error=/home/fenosoa/projects/def-salehlab-ab/fenosoa/code_source_for_Pipeline/Pipeline_For_TMA_Xenium/logs/run_pipeline_TMA_%j.err
